217daea2188973c98db55083009d2ec35163ed21
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2023-05-27 02:11:36 -0300

    feat: adding database connection configuration

---
+++ HairHub/db/ConnDB.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace HairHub.db
+{
+    class ConnDB
+    {
+        private MySqlConnection connectionString = new MySqlConnection(
+                "server=aws.connect.psdb.cloud;" +
+            "User Id=ophhbmiu03mqrm5rbcpf;database=hairhubdb; " +
+            "password=pscale_pw_QjFvyCbizFKhEypqHJPwbDlr6T3PzCjmJSVWjOhflZw"
+            );
+
+        public MySqlConnection getConnection()
+        {
+            if(this.connectionString.State == ConnectionState.Open)
+            {
+                this.connectionString.Close();
+            }
+
+            return this.connectionString;
+        }
+    }
+}
