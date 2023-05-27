05368cd4e865ae8fc1810080986335f4c4669051
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2023-05-27 09:59:23 -0300

    fix: adjusting encapsulation

--- HairHub/db/ConnDB.cs
+++ HairHub/db/ConnDB.cs
@@ -6,7 +6,7 @@ namespace HairHub.db
 {
     class ConnDB
     {
-        public static MySqlConnection connectionString = new MySqlConnection(
+        private static MySqlConnection connectionString = new MySqlConnection(
                 "server=aws.connect.psdb.cloud;" +
             "User Id=ophhbmiu03mqrm5rbcpf;database=hairhubdb; " +
             "password=pscale_pw_QjFvyCbizFKhEypqHJPwbDlr6T3PzCjmJSVWjOhflZw"
