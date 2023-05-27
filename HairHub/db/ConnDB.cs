e79da9535a2cf49ad0954de81e1946dc77055c1d
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2023-05-27 12:42:24 -0300

    chore: updating connection string

--- HairHub/db/ConnDB.cs
+++ HairHub/db/ConnDB.cs
@@ -8,8 +8,8 @@ namespace HairHub.db
     {
         private static MySqlConnection connectionString = new MySqlConnection(
                 "server=aws.connect.psdb.cloud;" +
-            "User Id=ophhbmiu03mqrm5rbcpf;database=hairhubdb; " +
-            "password=pscale_pw_QjFvyCbizFKhEypqHJPwbDlr6T3PzCjmJSVWjOhflZw"
+            "User Id=atjvoax3y61mv6n7m5cf;database=hairhubdb; " +
+            "password=pscale_pw_5GZ2qlzeC4mPx4hqObyYNbMMEp8hiipcNMv6BkWIv1o"
             );
 
         public static MySqlConnection openConnection()
