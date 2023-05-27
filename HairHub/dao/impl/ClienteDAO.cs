fe0290b42c1e26a953f5875625083cf42debd1c2
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2023-05-27 19:31:23 -0300

    fix: add alias

--- HairHub/dao/impl/ClienteDAO.cs
+++ HairHub/dao/impl/ClienteDAO.cs
@@ -141,7 +141,7 @@ namespace HairHub.dao.impl
             StringBuilder response = new StringBuilder();
             MySqlDataReader dr = null;
 
-            sql.Append("SELECT DISTINCT C.NOME, C.TELEFONE, SV.NOME, SV.VALOR ");
+            sql.Append("SELECT DISTINCT C.NOME, C.TELEFONE, SV.NOME AS NOMESV, SV.VALOR ");
             sql.Append("FROM AGENDAMENTO AG ");
             sql.Append("INNER JOIN CLIENTE C ON C.ID = AG.ID_CLIENTE ");
             sql.Append("INNER JOIN SERVICO SV ON SV.ID = AG.ID_SERVICO ");
