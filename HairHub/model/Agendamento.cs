da0dd315c6d0c2816f3e5bed26e4836f3470bb6c
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2023-05-27 14:03:42 -0300

    feat: creating AgendamentoDAO

--- HairHub/model/Agendamento.cs
+++ HairHub/model/Agendamento.cs
@@ -6,19 +6,21 @@ using System.Threading.Tasks;
 
 namespace HairHub.model
 {
-    class Agendamento
+    class Agendamento: EntidadeDominio
     {
-        public DateTime Data { get; set; }
-        public DateTime Hora { get; set; }
+        public string Data { get; set; }
+        public string Hora { get; set; }
         public int IdServico { get; set; }
         public int IdCliente { get; set; }
 
-        public Agendamento(DateTime data, DateTime hora, int id_servico, int id_cliente)
+        public Agendamento() { }
+
+        public Agendamento(string data, string hora, int idServico, int idCliente)
         {
-            this.Data = data;
-            this.Hora = hora;
-            this.IdServico = id_servico;
-            this.IdCliente = id_cliente;
+            Data = data;
+            Hora = hora;
+            IdServico = idServico;
+            IdCliente = idCliente;
         }
     }
 }
