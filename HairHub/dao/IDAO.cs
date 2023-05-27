cad25470a0dd6662e4cdccd2ef938559a4fac689
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2023-05-27 12:43:13 -0300

    feat: adding ClienteDAO CRUD

---
+++ HairHub/dao/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairHub.dao
+{
+    interface IDAO<T>
+    {
+        string Create(T entidade);
+
+        string Update(T entidade);
+
+        string Delete(T entidade);
+
+        MySqlDataReader FindAll();
+
+        T FindById(int id);
+
+    }
+}
---
+++ HairHub/dao/impl/ClienteDAO.cs
@@ -0,0 +1,142 @@
+﻿using HairHub.db;
+using HairHub.model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairHub.dao.impl
+{
+    class ClienteDAO : IDAO<Cliente>
+    {
+        public string Create(Cliente cliente)
+        {
+            StringBuilder sql = new StringBuilder();
+            StringBuilder response = new StringBuilder();
+
+            sql.Append("INSERT INTO CLIENTE(NOME, TELEFONE) VALUES ("+cliente.Nome+","+cliente.Telefone+")");
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sql.ToString(), ConnDB.openConnection());
+                command.ExecuteNonQuery();
+                ConnDB.closeConnection();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                response.Append("Erro ao inserir cliente, por favor, tente novamente mais tarde!");
+                return response.ToString();
+            }
+
+            response.Append("Cliente inserido com sucesso!");
+            return response.ToString();      
+        }
+
+        public string Update(Cliente cliente)
+        {
+            StringBuilder sql = new StringBuilder();
+            StringBuilder response = new StringBuilder();
+
+            sql.Append("UPDATE CLIENTE SET NOME = " + cliente.Nome + ", TELEFONE = " + cliente.Telefone +" ");
+            sql.Append("WHERE ID = "+cliente.Id);
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sql.ToString(), ConnDB.openConnection());
+                command.ExecuteNonQuery();
+                ConnDB.closeConnection();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                response.Append("Erro ao atualizar cliente, por favor, tente novamente mais tarde!");
+                return response.ToString();
+            }
+
+            response.Append("Cliente atualizado com sucesso!");
+            return response.ToString();
+        }
+
+        public string Delete(Cliente cliente)
+        {
+            StringBuilder sql = new StringBuilder();
+            StringBuilder response = new StringBuilder();
+
+            sql.Append("DELETE FROM CLIENTE ");
+            sql.Append("WHERE ID = " + cliente.Id);
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sql.ToString(), ConnDB.openConnection());
+                command.ExecuteNonQuery();
+                ConnDB.closeConnection();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                response.Append("Erro ao deletar cliente, por favor, tente novamente mais tarde!");
+                return response.ToString();
+            }
+
+            response.Append("Cliente excluído com sucesso!");
+            return response.ToString();
+        }
+
+        public MySqlDataReader FindAll()
+        {
+            StringBuilder sql = new StringBuilder();
+            StringBuilder response = new StringBuilder();
+            MySqlDataReader dr = null;
+
+            sql.Append("SELECT * FROM CLIENTE");
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sql.ToString(), ConnDB.openConnection());
+                dr = command.ExecuteReader();
+                ConnDB.closeConnection();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                response.Append("Erro ao deletar cliente, por favor, tente novamente mais tarde!");
+            }
+
+            return dr;
+        }
+
+        public Cliente FindById(int id)
+        {
+            StringBuilder sql = new StringBuilder();
+            StringBuilder response = new StringBuilder();
+            Cliente cliente = null;
+
+            sql.Append("SELECT ID, NOME, TELEFONE FROM CLIENTE WHERE ID = ?");
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sql.ToString(), ConnDB.openConnection());
+                command.Parameters.AddWithValue("@ID", id);
+                MySqlDataReader dr = command.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    cliente.Id = Convert.ToInt32(dr[0].ToString());
+                    cliente.Nome = dr[1].ToString();
+                    cliente.Telefone = dr[2].ToString();
+                }
+
+                ConnDB.closeConnection();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                response.Append("Erro ao deletar cliente, por favor, tente novamente mais tarde!");
+            }
+            return cliente;
+        }
+
+    }
+}
