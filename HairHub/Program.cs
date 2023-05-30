2a319a13f6917443f6bf44bd0e5b7e6941995688
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2023-05-30 20:14:15 -0300

    fix: FormMain to Form1

--- HairHub/Program.cs
+++ HairHub/Program.cs
@@ -16,7 +16,7 @@ namespace HairHub
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new Form1());
         }
     }
 }
